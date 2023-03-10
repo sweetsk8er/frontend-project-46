a7c17b9ad7162eac12ebadba9c1949f5df8dc47c
sweetsk8er <cranefront@yandex.ru>
2023-03-10 15:32:15 +0300

    step 3 again

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -15,4 +15,4 @@ program
     console.log(genDiff(filepath1, filepath2));
   });
 
-program.parse(process.argv);
+program.parse();
--- src/index.js
+++ src/index.js
@@ -4,28 +4,19 @@ import _ from 'lodash';
 const genDiff = (data1, data2) => {
   const keys = Object.keys({ ...data1, ...data2 });
   const sortKeys = _.sortBy(keys);
-  const diff = {};
-  for (const key of sortKeys) {
+  const result = sortKeys.map((key) => {
     const val1 = data1[key];
     const val2 = data2[key];
     if ((val1 !== undefined) && !val2) {
-      diff[`- ${key}`] = val1;
-    } else if (!val1 && (val2 !== undefined)) {
-      diff[`+ ${key}`] = val2;
-    } else if (val1 && val2 && (val1 === val2)) {
-      diff[`  ${key}`] = val1;
-    } else if (val1 && val2 && (val1 !== val2)) {
-      diff[`- ${key}`] = val1;
-      diff[`+ ${key}`] = val2;
+      return `- ${key}: ${val1}`;
+    } if (!val1 && (val2 !== undefined)) {
+      return `+ ${key}: ${val2}`;
+    } if (val1 && val2 && (val1 === val2)) {
+      return `  ${key}: ${val1}`;
     }
-  }
-  const result = JSON.stringify(diff)
-    .replaceAll('"', '')
-    .replaceAll(',', '\n  ')
-    .replaceAll('{', '{\n  ')
-    .replaceAll('}', '\n}');
-
-  return result;
+    return (`- ${key}: ${val1}\n+ ${key}: ${val2}`);
+  });
+  return `{\n${result.join('\n')}\n}`;
 };
 
 export default (filepath1, filepath2) => {
@@ -34,5 +25,5 @@ export default (filepath1, filepath2) => {
   const parsedData1 = JSON.parse(formatedData1);
   const parsedData2 = JSON.parse(formatedData2);
 
-  console.log(genDiff(parsedData1, parsedData2));
+  return genDiff(parsedData1, parsedData2);
 };
