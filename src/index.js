c184c976f5c60dcc148f4bdf515f76f1ca20b429
sweetsk8er <cranefront@yandex.ru>
2023-03-23 19:46:21 +0300

    правки намба туууууу

--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@ const fullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).substring(1);
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const getData = (filepath) => {
-    const patch = fullPath(filepath);
-    return parse(readFileSync(patch), extractFormat(filepath));
-  };
-  const data1 = getData(filepath1);
-  const data2 = getData(filepath2);
-  const tree = buildTree(data1, data2);
+  // получили путь
+  const pathfile1 = fullPath(filepath1);
+  const pathfile2 = fullPath(filepath2);
+  // прочитали файл
+  const dataFile1 = readFileSync(pathfile1);
+  const dataFile2 = readFileSync(pathfile2);
+  // прочитали формат
+  const formatFile1 = extractFormat(filepath1);
+  const formatFile2 = extractFormat(filepath2);
+
+  const tree = buildTree(
+    parse(dataFile1, formatFile1),
+    parse(dataFile2, formatFile2),
+  );
   return chooseFormat(tree, format);
 };
 
